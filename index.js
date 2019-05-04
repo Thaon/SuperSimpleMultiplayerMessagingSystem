cb44445ae66a1529e5d1113a022128308381f49a
YAP! <gabriele.maddaloni@gmail.com>
2019-05-04 17:17:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require("express")();
 var server = require("http").Server(app);
-var io = require("socket.io"(server));
+var io = require("socket.io")(server);
 
 server.listen(80);
 app.use(express.static("static"))
