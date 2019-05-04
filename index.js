d1a00bea52d33fc07a54631af246fc7ec198867e
YAP! <gabriele.maddaloni@gmail.com>
2019-05-04 20:54:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -46,10 +46,13 @@ io.on("connection", function(socket){
 
 	socket.on("get rooms", function(){
 		var rms = [];
+
+		console.log("Rooms requested, sending back " + rms.length + " rooms");
+
 		rooms.forEach(function(tRoom){
+			console.log(tRoom.name + " - " + tRoom.maxPlayers);
 			rms.push(tRoom);
 		})
-		console.log("Rooms requested, sending back " + rms.length + " rooms");
 		socket.emit("rooms received", rms);
 		console.log("Sent!");
 	})
