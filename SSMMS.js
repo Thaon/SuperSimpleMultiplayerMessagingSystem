0bdb9516e527f4e09c030c68982e24d80243fa34
YAP! <gabriele.maddaloni@gmail.com>
2019-05-04 17:07:52 +0800

    full functionality implemented, no testing

--- SSMMS.js
+++ SSMMS.js
@@ -3,15 +3,29 @@ var scriptTag = document.createElement("SocketIO");
 scriptTag.src = "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.slim.js";
 location.appendChild(scriptTag);
 
-var SSMMS = function()
+var SSMMS = function(debug)
 {
+	this.debug = debug;
 	this.socket = io();
 
 	this.socket.on("info", function(message){
-		console.log(message);
+		if (this.debug)
+			console.log(message);
 	})
 
-	this.CreateRoom = function(roomName){
-		
+	this.CreateRoom = function(roomName, maxPlayers){
+		this.socket.emit("create room", this.socket, roomName, maxPlayers);
+	}
+
+	this.JoinRoom = function(roomName){
+		this.socket.emit("join room", this.socket, roomName);
+	}
+
+	this.JoinEmptyRoom = function(){
+		this.socket.emit("join empty room", this.socket);
+	}
+
+	this.SendMessage = function(type, message){
+		this.socket.emit("message", this.socket, type, message);
 	}
 }
--- index.js
+++ index.js
@@ -8,9 +8,12 @@ app.get("/", function(req, res){
 	res.send(/SSMMS.js);
 })
 
+
+//vars-----------------------------------------------------------
 var rooms = [];
 
 
+//messages-----------------------------------------------------------
 io.on("connection", function(socket){
 	socket.emit("info", "Welcome to the SSMMS server!");
 	console.log("user connected: " + socket.id);
@@ -18,22 +21,87 @@ io.on("connection", function(socket){
 
 io.on("disconnect", function(socket){
 	//check if the room this socket was in is empty and remove it
+	rooms.forEach(function(room){
+		if (room.players.includes(socket))
+			room.players.pop(socket);
+
+		if(rooms.players.length == 0)
+			rooms.pop(room);
+	})
+
 	console.log("user disconnected: " + socket.id);
 })
 
-io.on("create room", function(roomName, maxPlayers){
+io.on("create room", function(socket, roomName, maxPlayers){
+	//create a new room if it's possible
+	var tRoom = null;
 
+	rooms.forEach(function(room){
+		if (room.name == roomName)
+			tRoom = room;
+	})
+
+	if(tRoom == null)
+	{
+		var tempRoom = {"name":roomName, "maxPlayers":maxPlayers, "players":[]}
+		tempRoom.players.push(socket);
+		tRoom.push(tempRoom);
+		socket.emit("info", "Room has been created!");
+	}
+	else
+		socket.emit("info", "Sorry but a room with the same name already exists");
 })
 
 io.on("join room", function(socket, roomName){
 	//join an existing room
+	var tRoom = null;
+
+	rooms.forEach(function(room){
+		if (room.name == roomName)
+			tRoom = room;
+	})
+
+	if(tRoom == null)
+	{
+		socket.emit("info", "Sorry, The room you are looking for does not exist :/");
+	}
+	else
+	{
+		if (tRoom.players.length < tRoom.maxPlayers)
+		{
+			tRoom.players.push(socket);
+			socket.emit("info", "The room has been joined successfully!");
+			
+		}
+		socket.emit("info", "Sorry but a room with the same name already exists");
+	}
 })
 
-io.on("join random room", function(socket){
+io.on("join empty room", function(socket){
 	//find a random room to join that is not full
+	rooms.forEach(function(room){
+		if (room.players.length < room.maxPlayers)
+		{
+			tRoom.players.push(socket);
+			socket.emit("info", "The first empty room has been joined successfully!");
+		}
+	})
 })
 
 io.on("message", function(socket, type, payload){
 	//rely a message to all sockets in the same room
+	var tRoom = null;
 
+	rooms.forEach(function(room){
+		if (room.players.includes(socket))
+			tRoom = room;
+	})
+
+	if (tRoom != null)
+	{
+		tRoom.players.forEach(function(socket)
+		{
+			socket.emit("message", type, payload);
+		})
+	}
 })
